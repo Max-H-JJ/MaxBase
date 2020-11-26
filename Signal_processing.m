50a69e97128653e10ccf255dec4701beb7cbab38
Maxhu <maxhu990609@gmail.com>
2020-11-26 15:23:10 +0800

    CW modified

--- Signal_processing.m
+++ Signal_processing.m
@@ -38,18 +38,6 @@ square_muldiv_v = ones(6,1,10);
 second_muldiv_v = ones(6,3,10);
 fourth_muldiv_v = ones(6,5,10);
 
-%muldiv method output
-muldiv_linear_average_output = ones(6,21);
-muldiv_squre_average_output = ones(6,21);
-muldiv_second_average_output = ones(6,21);
-muldiv_fourth_average_output = ones(6,21);
-
-%polyfit method output
-polyfit_linear_average_output = ones(6,21);
-polyfit_squre_average_output = ones(6,21);
-polyfit_second_average_output = ones(6,21);
-polyfit_fourth_average_output = ones(6,21);
-
 %initial the noisy results
 for i = 1:10
     
@@ -63,16 +51,16 @@ for i = 1:10
    for k = 1:6;
    %polyfit() method
    linear_polyfit_coefficient(k,:,i) = polyfit(I,P_noisy(k,:),1);
-   linear_polyfit_value(k,:,i) = polyval(linear_polyfit_coefficient(k,:,i),I);
+   linear_polyfit_value(k,:,i) = polyval([linear_polyfit_coefficient(k,:,i)],I);
    
 %    square_coefficient(k) = polyfit(I,P_noisy(k,:),2);
 %    square_polyfit_value(k)=polyval(square_polyfit_coefficient(k),I); 
    
    second_polyfit_coefficient(k,:,i) = polyfit(I,P_noisy(k,:),2);
-   second_polyfit_value(k,:,i) = polyval(second_polyfit_coefficient(k,:,i),I);
+   second_polyfit_value(k,:,i) = polyval([second_polyfit_coefficient(k,:,i)],I);
    
    fourth_polyfit_coefficient(k,:,i) = polyfit(I,P_noisy(k,:),4);
-   fourth_polyfit_value(k,:,i) = polyval(fourth_polyfit_coefficient(k,:),I);
+   fourth_polyfit_value(k,:,i) = polyval([fourth_polyfit_coefficient(k,:)],I);
    
    
    %premutiply method
@@ -85,33 +73,50 @@ for i = 1:10
    %v = X/Y
    for m1 = 1:2;        
       linear_muldiv_v(k,:,i)= linear_muldiv_X(:,m1,i)\P_data(k,:,i)';
-      linear_muldiv_value(k,:,i)=polyval(linear_muldiv_v(k,:,i),I);
+      linear_muldiv_value(k,:,i)=polyval([linear_muldiv_v(k,:,i)],I);
    end
         square_muldiv_v(k,:,i)=square_muldiv_X(:,:,i)\P_data(k,:,i)';
-        square_muldiv_value(k,:,i)=polyval(square_muldiv_v(k,:,i),I);
+        square_muldiv_value(k,:,i)=polyval([square_muldiv_v(k,:,i),0,0],I);
    for m2 = 1:3;
        second_muldiv_v(k,:,i)=second_muldiv_X(:,m2,i)\P_data(k,:,i)';
-       second_muldiv_value(k,:,i)=polyval(second_muldiv_v(k,:,i),I);
+       second_muldiv_value(k,:,i)=polyval([second_muldiv_v(k,:,i)],I);
    end
    for m4 = 1:5;
         fourth_muldiv_v(k,:,i)=fourth_muldiv_X(:,m4,i)\P_data(k,:,i)';
-        fourth_muldiv_value(k,:,i)=polyval(fourth_muldiv_v(k,:,i),I);
+        fourth_muldiv_value(k,:,i)=polyval([fourth_muldiv_v(k,:,i)],I);
    end
- %compute the average evaluated output
-   muldiv_linear_average_output(k,:) = sum(linear_muldiv_value(:,:,i))/10;
-   muldiv_squre_average_output(k,:)= sum(square_muldiv_value(:,:,i))/10;
-   muldiv_second_average_output(k,:)= sum(second_muldiv_value(:,:,i))/10;
-   muldiv_fourth_average_output(k,:)= sum(fourth_muldiv_value(:,:,i))/10;
-   
-   polyfit_linear_average_output(k,:) = sum(linear_polyfit_value(:,:,i))/10;
-   polyfit_squre_average_output(k,:)= sum(square_polyfit_value(:,:,i))/10;
-   polyfit_second_average_output(k,:)= sum(second_polyfit_value(:,:,i))/10;
-   polyfit_fourth_average_output(k,:)= sum(fourth_polyfit_value(:,:,i))/10;  
    end
 end
-%compute error
-%muldiv_linear_error = sum(muldiv_linear_average_output)/6;
 
+%compute the average output for 10 iterations
+P_data_average = zeros(6,21);
+linear_muldiv_value_average = zeros(6,21);
+second_muldiv_value_average = zeros(6,21);
+square_muldiv_value_average = zeros(6,21);
+fourth_muldiv_value_average = zeros(6,21);
 
+linear_polyfit_value_average = zeros(6,21);
+second_polyfit_value_average = zeros(6,21);
+fourth_polyfit_value_average = zeros(6,21);
+for i = 1:10;
+    for k = 1:6;
+    P_data_average(k,:) = P_data_average(k,:)+P_data(k,:,i)/10;
+    %muldiv average evaluation value
+    linear_muldiv_value_average(k,:) = linear_muldiv_value_average(k,:)+linear_muldiv_value(k,:,i)/10;
+    second_muldiv_value_average(k,:) = second_muldiv_value_average(k,:)+second_muldiv_value(k,:,i)/10;
+    square_muldiv_value_average(k,:) = square_muldiv_value_average(k,:)+square_muldiv_value(k,:,i)/10;
+    fourth_muldiv_value_average(k,:) = fourth_muldiv_value_average(k,:)+fourth_muldiv_value(k,:,i)/10;
+    
+    %polyfit average evaluation value
+    linear_polyfit_value_average(k,:) = linear_polyfit_value_average(k,:)+linear_polyfit_value(k,:,i)/10;
+    second_polyfit_value_average(k,:) = second_polyfit_value_average(k,:)+second_polyfit_value(k,:,i)/10;
+    fourth_polyfit_value_average(k,:) = fourth_polyfit_value_average(k,:)+fourth_polyfit_value(k,:,i)/10;
+    end 
+end
 
-%polyval()
+%least square fit = muldiv method
+linear_error = sum((linear_muldiv_value_average' - P_data_average').^2);
+square_error = sum((square_muldiv_value_average' - P_data_average').^2);
+second_error = sum((second_muldiv_value_average' - P_data_average').^2);
+fourth_error = sum(fourth_muldiv_value_average' - P_data_average').^2);
+
